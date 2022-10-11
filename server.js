0330ec458cfb59d9fc5d63bc9b458763c908fbf7
Norman <whosnorm@gmail.com>
2022-10-11 19:25:24 -0400

    working on edit.jsx

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require(`express`)
 const morgan = require(`morgan`)
 const pokemon = require(`./models/pokemon`)
-// const mongoose = require(`mongoose`)
+const mongoose = require(`mongoose`)
 // const methodOverride = require("method-override");
 // require(`dotenv`).config()
 
@@ -13,6 +13,7 @@ const app = express();
 app.use(morgan(`dev`));
 //Middleware to read post request
 app.use(express.urlencoded({ extended: false }));
+// app.use(express.json())
 
 // app.use(methodOverride("_method"));
 
@@ -35,24 +36,68 @@ app.get(`/pokemon`, (req,res)=>{
     res.render(`Index`, {pokemon:pokemon})
 })
 
+app.get('/pokemon/new', (req, res) => {
+    res.render('New', {pokemon:pokemon})
+});
+
 app.get(`/pokemon/:id`, (req,res)=>{
     const {id} = req.params
     res.render(`Show`, {pokemon:pokemon[id]})
 })
 
-//DELETE
-// app.delete("/fruits/:id", (req, res) => {
-    // console.log(`deleting...`);
-    // const { id } = req.params;
+//Edit
+app.get(`/pokemon/:id/edit`, (req,res)=>{
+    const {id} = req.params
+
+    //find the fruit in the db using the id
+    // Fruit.findById(id, (error, foundFruit) => {
+    //     if(error){
+    //         console.log(error)
+    //         res.status(403).send(`id not found...`)
+    //     }
+        //render the view and pass the data from the fruit
+        res.render(`Edit`, {pokemon:pokemon[id]} )
+    })  
+
+
+app.put(`/pokemon/:id/`, (req,res)=>{
+         const {id} = req.params
+        // pokemon[id].push(req.body)
+        //   Fruit.findByIdAndUpdate(id, req.body,(error, foundFruit) =>{
+        //     if(error){
+        //         console.log(error)
+        //         res.status(403).send(`id not found...`)
+        //     }
+        res.redirect(`/pokemon/`)
+})
+
+
+
+// app.get("/pokemon/new", (req, res) => {
+//     res.render("New", {pokemon:pokemon});
+//   });
+app.post(`/pokemon`, (req, res) => {
+    pokemon.push(req.body)
+    console.log(req.body)
+    res.redirect("/pokemon");
+});
+      
+
+
+// DELETE
+app.delete("/fruits/:id", (req, res) => {
+    console.log(`deleting...`);
+    const { id } = req.params;
+    pokemon.splice(id, 1)
     // Fruit.findByIdAndRemove(id, (err, data) => {
     //   console.log(data);
     //   if (err) {
     //     console.log(err);
     //     res.status(403).send(`Bad request`);
     //   }
-    //   res.redirect(``);
-    // });
-//   });
+      res.redirect(`/pokemon`);
+    
+  });
   
 
 // Tell the app to listen on port 3000
--- views/Index.jsx
+++ views/Index.jsx
@@ -12,12 +12,21 @@ class Index extends React.Component {
     return (
       <div style={styles}>
         <h1>See All The Pokemon</h1>
+        <nav>
+          <a href="/pokemon/new">Create a New Pokemon</a>
+        </nav>
+        {console.log(pokemon)}
         <ul>
           {pokemon.map((pokemon, idx) => (
             <li key={idx}>
               <img src={pokemon.img} />
               <br/>
               <a href={`/pokemon/${idx}`}>{cap(pokemon.name)}</a>
+              <br/>
+              <a href={`/pokemon/${idx}/edit`}>Edit this pokemon</a>
+              <form action={`/pokemon/${idx}?_method=DELETE`} method='POST'>
+                  <input type='submit' value='Delete' />
+              </form>
             </li>
           ))}
         </ul>
